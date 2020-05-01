3062fd618aa4e352d2006ead87315b8a47c456e6
Nial <nial.tilson@gmail.com>
2020-05-01 11:09:19 +0200

    Update app.js
    Navbar and Footer callouts at top
    
    Uncomment img carousel

--- app.js
+++ app.js
@@ -1,20 +1,5 @@
-// var myIndex = 0;
-// img_carousel();
-//
-// function img_carousel() {
-//   var i;
-//   var x = document.getElementsByClassName("full");
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   myIndex++;
-//   if (myIndex > x.length) {myIndex = 1}
-//   x[myIndex-1].style.display = "block";
-//   setTimeout(img_carousel,3000);
-// }
-
 //Main Navigation Menu Callout
-$.get("navbar.html", function(data){
+$.get("https://nialtilson.github.io/navbar.html", function(data){
   $("#nav-placeholder").replaceWith(data);
 });
 
@@ -37,3 +22,18 @@ $.get("https://nialtilson.github.io/navbar_dhwga.html", function(data){
 $.get("https://nialtilson.github.io/footer.html", function(data){
   $("#footer").replaceWith(data);
 });
+
+var myIndex = 0;
+img_carousel();
+
+function img_carousel() {
+  var i;
+  var x = document.getElementsByClassName("full");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(img_carousel,3000);
+}
