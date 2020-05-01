d642906afa1c144bc97ab47f7f0cd5a82423171d
Nial <nial.tilson@gmail.com>
2020-05-01 09:29:42 +0200

    Update app.js
    Removed other JQuery calls

--- app.js
+++ app.js
@@ -18,23 +18,23 @@ $.get("navbar.html", function(data){
   $("#nav-placeholder").replaceWith(data);
 });
 
-//Main Navigation Menu Callout for Deutschland, Here We Go! Pages
-$.get("navbar_dhwg.html", function(data){
-  $("#nav-placeholder-dhwg").replaceWith(data);
-});
-
-//Main Navigation Menu Callout for Wandering the World with Nial Pages
-$.get("navbar_wtwwn.html", function(data){
-  $("#nav-placeholder-wtwwn").replaceWith(data);
-});
-
-//Main Navigation Menu Callout for Deutschland, Here We Go Again! Pages
-$.get("navbar_dhwga.html", function(data){
-  $("#nav-placeholder-dhwga").replaceWith(data);
-});
-
-
-//Footer Callout for all pages
-$.get("footer.html", function(data){
-  $("#footer").replaceWith(data);
-});
+// //Main Navigation Menu Callout for Deutschland, Here We Go! Pages
+// $.get("navbar_dhwg.html", function(data){
+//   $("#nav-placeholder-dhwg").replaceWith(data);
+// });
+//
+// //Main Navigation Menu Callout for Wandering the World with Nial Pages
+// $.get("navbar_wtwwn.html", function(data){
+//   $("#nav-placeholder-wtwwn").replaceWith(data);
+// });
+//
+// //Main Navigation Menu Callout for Deutschland, Here We Go Again! Pages
+// $.get("navbar_dhwga.html", function(data){
+//   $("#nav-placeholder-dhwga").replaceWith(data);
+// });
+//
+//
+// //Footer Callout for all pages
+// $.get("footer.html", function(data){
+//   $("#footer").replaceWith(data);
+// });
